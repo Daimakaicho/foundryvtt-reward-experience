d6573a22d5e4d8f0ec64d2c3568043ec199b0ec2
Daimakaicho <cbouyer@protonmail.com>
2021-07-01 23:45:57 +0200

    add confirm dialog before rewarding experience

--- scripts/CombatManager.js
+++ scripts/CombatManager.js
@@ -1,36 +1,20 @@
 'use strict';
-import { TEMPLATES_PATH, REWARD_EXPERIENCE_CARD_TEMPLATE, TEMPLATES_EXTENSION, LEVELUP_CARD_TEMPLATE } from './config.js'
+import { TEMPLATES_PATH, REWARD_EXPERIENCE_CARD_TEMPLATE, TEMPLATES_EXTENSION, LEVELUP_CARD_TEMPLATE, REWARD_DIALOG_TEMPLATE } from './config.js'
 
 export class CombatManager {
     _onEndCombat(combatData) {
         if (!game.user.isGM) return;
+        this._createRewardDialog(combatData)
+    }
 
-        if (combatData === undefined) {
-            ui.notifications.warn(game.i18n.localize("reward-experience.error.no-active-combat"));
-            return;
-        }
-
-        const monsters = combatData.data.combatants.filter(x => !x.actor.hasPlayerOwner);
-        if (!monsters || monsters.length === 0) {
-            ui.notifications.warn(game.i18n.localize("reward-experience.error.no-monsters-in-combat"));
-            return;
-        }
-
-        const totalExperience = monsters.map(x => x.actor.data.data.details.xp.value).reduce((a, b) => a + b);
-        // If monsters give no experience, we won't go further
-        if (totalExperience === 0) return;
-
-
-        const players = combatData.data.combatants.filter(x => x.actor.hasPlayerOwner);
-        if (!players || players.length === 0) {
-            ui.notifications.warn(game.i18n.localize("reward-experience.error.no-players-in-combat"));
-            return;
-        }
-
-        const characterExperience = Math.floor(totalExperience / players.length);
-        // If monsters give so fex experience that rounded per charecter is 0, we won't go further
-        if (characterExperience === 0) return;
-
+    /**
+     * Share experience between all players for an ending combat.
+     *
+     * @param {Combatant} combatant       Combatant in combat
+     *
+     * @return {Boolean}                  Returns true if combatant is owned by a player, false otherwise.
+     */
+    async _shareExperience(players, characterExperience) {
         for (const player of players) {
             // Getting some player's experience data
             const playerExperienceData = player.actor.data.data.details.xp;
@@ -41,15 +25,7 @@ export class CombatManager {
             const newExperienceValue = currentExperience + characterExperience;
 
             // We apply experience gain to player
-            player.actor.update({
-                data: {
-                    details: {
-                        xp: {
-                            value: newExperienceValue
-                        }
-                    }
-                }
-            });
+            player.actor.update({ "data.details.xp.value": newExperienceValue });
 
             // We send a whisper to player with experience earned.
             // If player can level up, another whisper is send as warning.
@@ -60,6 +36,74 @@ export class CombatManager {
                 }
             });
         }
+    }
+
+    /**
+     * Is combatant owned by a player ?
+     *
+     * @param {Combatant} combatant       Combatant in combat
+     *
+     * @return {Boolean}                  Returns true if combatant is owned by a player, false otherwise.
+     */
+    _isPlayer(combatant) {
+        return combatant.hasPlayerOwner;
+    }
+
+    /**
+     * Show dialog asking to GM is experience points should be rewarded to player or not.
+     *
+     * @param {CombatData} combatData     Data of the ending combat
+     * 
+     */
+    async _createRewardDialog(combatData) {
+        if (!game.user.isGM) return;
+
+        if (combatData === undefined) {
+            ui.notifications.warn(game.i18n.localize("reward-experience.error.no-active-combat"));
+            return;
+        }
+
+        const monsters = combatData.combatants.filter(x => !this._isPlayer(x));
+        if (!monsters || monsters.length === 0) {
+            ui.notifications.warn(game.i18n.localize("reward-experience.error.no-monsters-in-combat"));
+            return;
+        }
+
+        const totalExperience = monsters.map(x => x.actor.data.data.details.xp.value).reduce((a, b) => a + b);
+        // If monsters give no experience, we won't go further
+        if (totalExperience === 0) return;
+
+
+        const players = combatData.combatants.filter(x => this._isPlayer(x));
+        if (!players || players.length === 0) {
+            ui.notifications.warn(game.i18n.localize("reward-experience.error.no-players-in-combat"));
+            return;
+        }
+
+        const characterExperience = Math.floor(totalExperience / players.length);
+        // If monsters give so fex experience that rounded per charecter is 0, we won't go further
+        if (characterExperience === 0) return;
+
+        return renderTemplate(`${TEMPLATES_PATH}/${REWARD_DIALOG_TEMPLATE}.${TEMPLATES_EXTENSION}`, {
+            players: players,
+            characterExperience: characterExperience,
+            totalExperience: totalExperience
+        }).then(content => {
+            return new Dialog({
+                title: game.i18n.localize("reward-experience.dialog.title"),
+                content: content,
+                buttons: {
+                    yes: {
+                        label: game.i18n.localize("reward-experience.dialog.buttons.confirm"),
+                        callback: async () => this._shareExperience(players, characterExperience)
+                    },
+                    no: {
+                        label: game.i18n.localize("reward-experience.dialog.buttons.cancel")
+                    }
+                },
+                default: "yes"
+            }).render(true);
+        });
     }
 
     /**
--- scripts/config.js
+++ scripts/config.js
@@ -2,5 +2,6 @@ export const MODULE_ID = "foundryvtt-reward-experience";
 export const MODULE_NAME = "Reward Experience";
 export const LEVELUP_CARD_TEMPLATE = "levelup-card";
 export const REWARD_EXPERIENCE_CARD_TEMPLATE = "reward-experience-card";
+export const REWARD_DIALOG_TEMPLATE = "reward-dialog";
 export const TEMPLATES_EXTENSION = "hbs";
 export const TEMPLATES_PATH = `/modules/${MODULE_ID}/templates`;
